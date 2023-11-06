5c17a23c413ee75b509da83422ad2806782fa23a
NeNorvalls <149901563+SASolem@users.noreply.github.com>
2023-11-06 04:32:05 +0100

    fixing

--- index.js
+++ index.js
@@ -24,3 +24,11 @@ document.getElementById("ContactForm").addEventListener("submit", function (even
         document.getElementById("messageError").textContent = "";
     }
 });
+
+const portfolioURL = 'https://nenorvalls-projects-showcase.netlify.app/';
+
+const goBackButton = document.getElementById('goBackButton');
+
+goBackButton.addEventListener('click', function() {
+  window.location.href = portfolioURL;
+});
