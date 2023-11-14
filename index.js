332d43afd7bd30c373d9eeeeaddbe9e6d81e8d65
Crystaliner1000 <honeygeeh27@gmail.com>
2023-11-14 08:25:59 +0100

    adding go back button

--- index.js
+++ index.js
@@ -1,34 +1,29 @@
-document.getElementById("ContactForm").addEventListener("submit", function (event) {
+document
+  .getElementById("ContactForm")
+  .addEventListener("submit", function (event) {
     var name = document.getElementById("name").value.trim();
     var email = document.getElementById("email").value.trim();
     var message = document.getElementById("message").value.trim();
 
     if (name === "") {
-        document.getElementById("nameError").textContent = "Name is required";
-        event.preventDefault();
+      document.getElementById("nameError").textContent = "Name is required";
+      event.preventDefault();
     } else {
-        document.getElementById("nameError").textContent = "";
+      document.getElementById("nameError").textContent = "";
     }
 
     if (email === "") {
-        document.getElementById("emailError").textContent = "Email is required";
-        event.preventDefault();
+      document.getElementById("emailError").textContent = "Email is required";
+      event.preventDefault();
     } else {
-        document.getElementById("emailError").textContent = "";
+      document.getElementById("emailError").textContent = "";
     }
 
     if (message === "") {
-        document.getElementById("messageError").textContent = "Message is required";
-        event.preventDefault();
+      document.getElementById("messageError").textContent =
+        "Message is required";
+      event.preventDefault();
     } else {
-        document.getElementById("messageError").textContent = "";
+      document.getElementById("messageError").textContent = "";
     }
-});
-
-const portfolioURL = 'https://nenorvalls-projects-showcase.netlify.app/';
-
-const goBackButton = document.getElementById('goBackButton');
-
-goBackButton.addEventListener('click', function() {
-  window.location.href = portfolioURL;
-});
+  });
